98e66683fc224ed6063d898da6f6555eb345aca6
sbryan19 <seabryan19@gmail.com>
2026-06-07 02:19:10 +0800

    Updates

--- src/core/celestials/ra/ra.js
+++ src/core/celestials/ra/ra.js
@@ -233,7 +233,7 @@ export const Ra = {
   remembrance: {
     multiplier: 5,
     nerf: 1.0,
-    requiredLevels: 20,
+    requiredLevels: 1,
     get isUnlocked() {
       return Ra.totalPetLevel >= this.requiredLevels;
     }
