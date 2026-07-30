fbab59a1ba3ad5f929d8e7b795c6b4304a5333a9
sbryan19 <seabryan19@gmail.com>
2026-07-30 21:11:28 +0800

    update game.js

--- src/game.js
+++ src/game.js
@@ -608,7 +608,7 @@ export function gameLoop(passDiff, options = {}) {
   }
 
   const infinitiedMilestone = getInfinitiedMilestoneReward(totalGameTime * 5);
-  const eternitiedMilestone = getEternitiedMilestoneReward(totalGameTime * 2);
+  const eternitiedMilestone = getEternitiedMilestoneReward(totalGameTime * 1);
 
    if (eternitiedMilestone.gt(0)) {
      Currency.eternities.add(eternitiedMilestone);}
