7237381ff497e28bb00bee292032c09aa51f817a
tsurugi <167219218+tsurugi1255@users.noreply.github.com>
2026-07-31 14:58:17 +0800

    Update replicanti-galaxy-autobuyer.js

--- src/core/autobuyers/replicanti-galaxy-autobuyer.js
+++ src/core/autobuyers/replicanti-galaxy-autobuyer.js
@@ -14,7 +14,7 @@ export class ReplicantiGalaxyAutobuyerState extends AutobuyerState {
   }
 
   get isEnabled() {
-    return Achievement(138).isUnlocked || !TimeStudy(131).isBought;
+    return true;
   }
 
   get hasUnlimitedBulk() {
